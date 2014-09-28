f8fc933b1375f49ab6fe9c5a86570ebc0dfad476
Julian Pineda <studio@miroamarillo.com>
2014-09-28 15:45:46 -0400

    Setting up questions for the user

--- generator-myyogenerator/app/index.js
+++ generator-myyogenerator/app/index.js
@@ -5,7 +5,33 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
 var MyyogeneratorGenerator = yeoman.generators.Base.extend({
+  promptUser: function(){
+      var done = this.async();
 
+      //Greetings from Yeoman
+      console.log(this.yeoman);
+
+      //Questions for the user
+      var prompts = [
+        {
+          name: 'appName',
+          message: 'What\'s your app\'s name?'
+        },
+        {
+          type: 'confirm',
+          name: 'addDemoSection',
+          message: 'Would you like to generate a demo section?',
+          default: true
+        }
+      ];
+
+      this.prompt(prompts, function(props){
+        this.appName = props.appName;
+        this.addDemoSection = props.addDemoSection;
+
+        done();
+      }.bind(this));
+  }
 });
 
 module.exports = MyyogeneratorGenerator;
